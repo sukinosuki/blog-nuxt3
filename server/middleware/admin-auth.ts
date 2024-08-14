8c03d3dc59c3cb1716d9204b28404eb5d1b5a934
hanami <1021210354@qq.com>
2024-08-14 15:39:26 +0800

    test deploy

--- server/middleware/admin-auth.ts
+++ server/middleware/admin-auth.ts
@@ -7,13 +7,14 @@ export default eventHandler(async (event) => {
   console.log('admin auth, runtimeConfig11 ', runtimeConfig)
 
   if (event.path.startsWith('/api-admin')) {
+    // would cause the Empty password error
     const userSession = await getUserSession(event).catch((err) => {
       console.log('get user session err ', err)
     })
 
     console.log('userSession ', userSession)
 
-    if (!userSession.user) {
+    if (!userSession?.user) {
       if (!['/api-admin/auth/login', '/api-admin/auth/login/'].includes(event.path)) {
         throw createError({
           status: 403,
