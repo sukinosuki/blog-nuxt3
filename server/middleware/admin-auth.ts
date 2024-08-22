22c1640813219327535fcd91f750d81df71a0e68
hanami <1021210354@qq.com>
2024-08-22 16:55:29 +0800

    test nuxt auth

--- server/middleware/admin-auth.ts
+++ server/middleware/admin-auth.ts
@@ -1,4 +1,4 @@
-import { getServerSession } from '#auth'
+// import { getServerSession } from '#auth'
 
 // nuxt auth接口校验, https://auth.sidebase.io/guide/authjs/server-side/session-access
 // NOTE:访问的任何地址都会经过该中间件, ge: /、/post、/admin/auth/login、/admin/post、/api/**、/api-admin/**...
